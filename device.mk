c7a4d27b850f19c3bbd80ea665a71c56655376f8
Michael Bestas <mkbestas@lineageos.org>
2019-01-08 01:51:52 +0200

    vayu: rootdir: Import init.qcom.early_boot.sh
    * We used to have an init extension, which breaks treble
      and since P it requires custom SELinux rules to allow
      init reading /proc/sys/kernel/boot_reason
    * Remove the init extension and set the property like
      QCOM does through init.qcom.early_boot.sh
    * Update the property with vendor prefix while we are at it
    
    Change-Id: I238f754a17c9a548c6c90503bfa4b21763d1b4c4
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- device.mk
+++ device.mk
@@ -237,6 +237,7 @@ PRODUCT_COPY_FILES += \
 # Init scripts
 PRODUCT_PACKAGES += \
     init.mi.usb.sh \
+    init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.sensors.sh \
     init.qcom.sh \
