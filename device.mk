39ce5285e7cb671001d2ebcb9479818fb8dc3d2d
Michael Bestas <mkbestas@lineageos.org>
2021-07-28 00:35:32 +0700

    vayu: Bring back AOSP BT stack

--- device.mk
+++ device.mk
@@ -41,7 +41,6 @@ PRODUCT_PACKAGES += \
     audio.primary.msmnile \
     audio.r_submix.default \
     audio.usb.default \
-    liba2dpoffload \
     libaudiopreprocessing \
     libbatterylistener \
     libbundlewrapper \
@@ -82,7 +81,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
     libldacBT_dec \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
