d2893e9eecee3c814515854100d12279dd14b394
InVictusXV <aradheakiki123@gmail.com>
2021-07-28 07:20:55 +0700

    vayu: ConquerOS bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_vayu.mk
+    $(LOCAL_DIR)/conquer_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_vayu-user \
-    derp_vayu-userdebug \
-    derp_vayu-eng
+    conquer_vayu-user \
+    conquer_vayu-userdebug \
+    conquer_vayu-eng
